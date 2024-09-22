72f1997b6d1db5e33f5b0f26473fc9afa5cb540c
abhishekspy007 <abhishekspy007@gmail.com>
2024-09-22 15:26:03 +0000

    first draft

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -4,7 +4,7 @@
   # Which nixpkgs channel to use.
   channel = "stable-23.11"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
-  packages = [ pkgs.nodejs_20 ];
+  packages = [ pkgs.nodejs_20 pkgs.bun ];
   # Sets environment variables in the workspace
   env = { };
   idx = {
